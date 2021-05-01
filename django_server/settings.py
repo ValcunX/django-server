ea796dd3003d7d1d5ce386ac80cd2f29f2cd8dea
nikhiltadikonda <nikhiltadikonda@gmail.com>
2021-05-01 12:08:02 +0530

    #2: Added Djongo to Django

--- django_server/settings.py
+++ django_server/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'django_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'ENGINE': 'djongo',
+         'NAME': 'valcunx',
+     }
 }
 
 
