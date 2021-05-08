eb70eab28d9f6493bb8ba5def270beed55e7e096
nikhiltadikonda <nikhiltadikonda@gmail.com>
2021-05-08 18:45:40 +0530

    #10 Added REST API to Django Backend

---
+++ backend/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from .models import Language, Project, User
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Language
+        fields = ('name', 'image_id')
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('name', 'user', 'language', 'volume_id')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'email')
+
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import LanguageSerializer, ProjectSerializer, UserSerializer
+from .models import Language, Project, User
+
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    queryset = Language.objects.all().order_by('name')
+    serializer_class = LanguageSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all().order_by('name')
+    serializer_class = ProjectSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('name')
+    serializer_class = UserSerializer
