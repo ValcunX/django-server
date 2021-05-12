b80719c518b010a6973b356cb94d922beee6adfb
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-05-12 22:45:12 +0530

    Added user projects url

--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import LanguageSerializer, ProjectSerializer, UserSerializer
 from .models import Language, Project, User
@@ -21,3 +23,10 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     http_method_names = ['get', 'post', 'put', 'delete']
+
+    @action(detail=True, methods=['get'])
+    def projects(self, request, pk=None):
+        user = self.get_object()
+        queryset = Project.objects.filter(user=user)
+        serializer = ProjectSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
