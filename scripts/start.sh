cd616e1be1ef033d94fcea9514e037d64bab3a6e
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-06-12 17:08:23 +0530

    Removed .env in build

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-docker run --rm -d --name valcunx_django-server -p 8000:80 vulcanx/django-server
+docker run --rm -d --name valcunx_django-server --env-file ./.env.prod -p 8000:80 vulcanx/django-server
